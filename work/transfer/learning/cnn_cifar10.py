5be50862dd7ecdda6551fcedb537c4f23e86c9d9
c10t <c10t@fa.ke>
2018-10-03 09:32:45 +0900

    data augmentation

--- work/transfer/learning/cnn_cifar10.py
+++ work/transfer/learning/cnn_cifar10.py
@@ -4,6 +4,7 @@
 import os
 
 import keras
+import numpy as np
 
 from keras.callbacks import ModelCheckpoint
 from keras.callbacks import TensorBoard
@@ -16,6 +17,7 @@ from keras.layers.core import Dropout
 from keras.layers.core import Flatten
 from keras.models import Sequential
 from keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
 
 
 def network(input_shape, num_classes):
@@ -71,6 +73,7 @@ class Trainer():
                              metrics=["accuracy"])
         self.verbose = 1
         self.log_dir = os.path.join(os.path.dirname(__file__), "logdir")
+        self.model_file_name = "model_file.hdf5"
 
     def train(self, x_train, y_train, batch_size, epochs, validation_split):
         if os.path.exists(self.log_dir):
@@ -79,10 +82,46 @@ class Trainer():
 
         os.mkdir(self.log_dir)
 
-        self._target.fit(
-          x_train, y_train,
-          batch_size=batch_size, epochs=epochs,
-          validation_split=validation_split,
+        """
+        01. set input mean to 0 over the dataset
+        02. set each sample mean to 0
+        03. divide inputs by std of the dataset
+        04. divide each input by its std
+        05. apply ZCA whitening
+        06. randomly rotate images in the range (degrees, 0 to 180)
+        07. randomly shift images horizontally (fraction of total width)
+        08. randomly shift images vertically (fraction of total height)
+        09. randomly flip images
+        10. randomly flip imagess
+        """
+        datagen = ImageDataGenerator(
+          featurewise_center=False,  # 01
+          samplewise_center=False,  # 02
+          featurewise_std_normalization=False,  # 03
+          samplewise_std_normalization=False,  # 04
+          zca_whitening=False,  # 05
+          rotation_range=0,  # 06
+          width_shift_range=0.1,  # 07
+          height_shift_range=0.1,  # 08
+          horizontal_flip=True,  # 09
+          vertical_flip=False  # 10
+        )
+
+        datagen.fit(x_train)  # compute quantities for normalization
+
+        indices = np.arange(x_train.shape[0])
+        np.random.shuffle(indices)
+        validation_size = int(x_train.shape[0] * validation_split)
+        x_train = x_train[indices[:-validation_size], :]
+        x_valid = x_train[indices[-validation_size:], :]
+        y_train = y_train[indices[:-validation_size], :]
+        y_valid = y_train[indices[-validation_size:], :]
+
+        self._target.fit_generator(
+          datagen.flow(x_train, y_train, batch_size=batch_size),
+          steps_per_epoch=x_train.shape[0] // batch_size,
+          epochs=epochs,
+          validation_data=(x_valid, y_valid),
           callbacks=[
             TensorBoard(log_dir=self.log_dir),
             ModelCheckpoint(
@@ -90,7 +129,8 @@ class Trainer():
               save_best_only=True
             )
           ],
-          verbose=self.verbose
+          verbose=self.verbose,
+          workers=4
         )
 
 
